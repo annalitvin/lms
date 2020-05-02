1d84e3c141b84036370063e8f6101af79f7927c9
annalitvin <litvin_any@ukr.net>
2020-05-02 12:58:20 +0300

    Fixed add_teacher command, now the argument <number_teacher> takes only one number

--- src/teacher/management/commands/add_teacher.py
+++ src/teacher/management/commands/add_teacher.py
@@ -7,16 +7,15 @@ class Command(BaseCommand):
     help = "Added new teachers in database"
 
     def add_arguments(self, parser):
-        parser.add_argument("number_teacher", nargs='+', type=int,
+        parser.add_argument("number_teacher", type=int,
                             help="Number of teachers to add to the database")
 
     def handle(self, *args, **options):
         number_teachers = options['number_teacher']
 
-        for number_teacher in number_teachers:
-            try:
-                for _ in range(number_teacher):
-                    Teacher.generate_teacher()
-            except Exception as ex:
-                raise CommandError(f'Data added fail! {ex}')
-            self.stdout.write(self.style.SUCCESS(f'Data added successfully! {number_teacher}'))
+        try:
+            for _ in range(number_teachers):
+                Teacher.generate_teacher()
+        except Exception as ex:
+            raise CommandError(f'Data added fail! {ex}')
+        self.stdout.write(self.style.SUCCESS(f'Data added successfully! {number_teachers}'))
