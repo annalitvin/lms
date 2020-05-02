906e329612802b82bdbd66111c798482d45018ca
annalitvin <litvin_any@ukr.net>
2020-05-02 12:55:56 +0300

    Fixed add_student command, now the argument <number_student> takes only one number

--- src/student/management/commands/add_student.py
+++ src/student/management/commands/add_student.py
@@ -7,16 +7,15 @@ class Command(BaseCommand):
     help = "Added new students in database"
 
     def add_arguments(self, parser):
-        parser.add_argument("number_student", nargs='+', type=int,
+        parser.add_argument("number_student", type=int,
                             help="Number of students to add to the database")
 
     def handle(self, *args, **options):
         number_students = options['number_student']
 
-        for number_student in number_students:
-            try:
-                for _ in range(number_student):
-                    Student.generate_student()
-            except Exception as ex:
-                raise CommandError(f'Data added fail! {ex}')
-            self.stdout.write(self.style.SUCCESS(f'Data added successfully! {number_student}'))
+        try:
+            for _ in range(number_students):
+                Student.generate_student()
+        except Exception as ex:
+            raise CommandError(f'Data added fail! {ex}')
+        self.stdout.write(self.style.SUCCESS(f'Data added successfully! {number_students}'))
