27250c1dd0fa81775c49276b2144ba277e2527ca
annalitvin <litvin_any@ukr.net>
2020-05-05 02:31:39 +0300

    Update HttpResponse text output in main page

--- src/app/views.py
+++ src/app/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 
 
 def main(request):
-    return HttpResponse("Generate teachers in database using url: <strong> <a href='/teacher'>/teacher</a> </strong> "
-                        "<br>" +
-                        "or generate students in database using url: <strong> <a href='/student'>/student</a> </strong>"
-                        + "<br><br>", status=200)
+    return HttpResponse("Get group by id using url: <strong> <a href='/teacher'>/teacher</a> </strong><br>"
+                        "Generate students in database using url: <strong> <a href='/student'>/student</a> </strong>" +
+                        "<br>Search students when all fields True using url: "
+                        "<strong> <a href='/student/students_list/1'>/student/students_list/1</a> </strong><br>" +
+                        "Search students when not all fields True using url: " +
+                        "<strong> <a href='/student/students_list/2'>/student/students_list/2</a> </strong><br>"
+                        "Get group by id using url: <strong> <a href='/group'>/group</a> </strong>",
+                        status=200)
