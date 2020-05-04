1ff7680f2349550b6194bbad0b7cbdb018fc49d0
annalitvin <litvin_any@ukr.net>
2020-05-04 23:26:44 +0300

    Added index page in group app

--- src/group/views.py
+++ src/group/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Group
+
+
+def index(request):
+    id_group = request.GET.get('id')
+
+    if id_group is not None:
+        if not id_group.isdigit():
+            return HttpResponse("'id' must be numeric and greatest when zero")
+
+        id_group = int(id_group)
+        try:
+            group = Group.objects.get(pk=id_group)
+        except Group.DoesNotExist:
+            raise Http404("Group does not exist")
+        return render(request, "group/index.html", {'group': group})
+    return HttpResponse("Введите параметр id: ?id=n")
