bdaedb8264c283674e78b7f8c740d27a36cb7556
annalitvin <litvin_any@ukr.net>
2020-05-12 13:14:03 +0300

    Added information about search teachers and search groups

--- src/app/views.py
+++ src/app/views.py
@@ -5,6 +5,8 @@ def main(request):
     return HttpResponse("Get teacher by id using url: <strong> <a href='/teacher'>/teacher</a> </strong><br>" +
                         "Added teacher: <strong> <a href='/teacher/add'>/teacher/add</a> </strong>"
                         + "<br>" +
+                        "Search teachers: <strong> <a href='/teacher/list'>/teacher/list/</a></strong>"
+                        + "<br>" +
                         "Generate students in database using url: <strong> <a href='/student'>/student</a> </strong>"
                         + "<br>" +
                         "Search students when all fields True using url: " +
@@ -14,5 +16,7 @@ def main(request):
                         "Added student: <strong> <a href='/student/add'>/student/add</a> </strong>"
                         + "<br>" +
                         "Get group by id using url: <strong> <a href='/group'>/group</a> </strong><br>" +
-                        "Added group: <strong> <a href='/group/add'>/group/add</a> </strong>",
+                        "Added group: <strong> <a href='/group/add'>/group/add</a> </strong>" +
+                        "<br>" +
+                        "Search groups: <strong> <a href='/group/groups_list'>/group/groups_list/</a></strong>",
                         status=200)
