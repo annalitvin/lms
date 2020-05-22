e8678c7aa72bd7e0bd6a32cd31469ade8846d1de
annalitvin <litvin_any@ukr.net>
2020-05-22 05:35:39 +0300

    Added groups in student genegation classmethod

--- src/student/models.py
+++ src/student/models.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 from django.db import models
 
@@ -11,22 +12,27 @@ from group.models import Group
 class Student(models.Model):
     first_name = models.CharField(max_length=40, null=False)
     last_name = models.CharField(max_length=20, null=False)
-    email = models.EmailField(max_length=50, null=False)
+    email = models.EmailField(max_length=50, null=False, db_index=True)
     birthdate = models.DateField(default=datetime.date.today)
     phone_number = models.CharField(max_length=30, null=True)
-    group = models.ForeignKey(to=Group, null=True, on_delete=models.SET_NULL)
+    group = models.ForeignKey(to=Group, null=True, on_delete=models.SET_NULL, related_name='students')
 
     def __str__(self):
         return f'{self.first_name}, {self.last_name}, {self.birthdate}'
 
     @classmethod
-    def generate_student(cls):
+    def generate_student(cls, groups=None):
         faker = Faker()
+
+        if groups is None:
+            groups = list(Group.objects.all())
 
         student = cls(
             first_name=faker.first_name(),
             last_name=faker.last_name(),
             email=faker.email(),
             phone_number=faker.phone_number(),
+            group=random.choice(groups)
+
         )
         student.save()
