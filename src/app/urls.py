b2fbad7822a9a3c298b6eb93b1ef9f3e31119881
annalitvin <litvin_any@ukr.net>
2020-05-30 11:18:27 +0300

    Added new add URL

--- src/app/urls.py
+++ src/app/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', views.main, name='main'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('account/', include('user_account.urls'), name='account'),
     path('teacher/', include('teacher.urls'), name='teacher'),
     path('student/', include('student.urls'), name='student'),
     path('group/', include('group.urls'), name='group')
