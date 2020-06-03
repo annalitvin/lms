170cee4f11d8e21a7a180d7f5101dfb4a9664927
annalitvin <litvin_any@ukr.net>
2020-06-03 08:35:26 +0300

    Problem solved in Student model

--- src/student/admin.py
+++ src/student/admin.py
@@ -12,9 +12,13 @@ class StudentAdminModel(admin.ModelAdmin):
     # https://stackoverflow.com/questions/163823/can-list-display-in-a-django-modeladmin-display-attributes-of-foreignkey-field
     # https://docs.djangoproject.com/en/2.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
 
+    # решено, но все таки не ясно почему до этого не работало, когда проверку 'if instance.group:' не делалась
+
     def get_group_name(self, instance):
-        print(instance.group.name)
-        return instance.group.name
+        if instance.group:
+            return instance.group.name
+
+    get_group_name.short_description = 'Group name'
 
 
 admin.site.register(Student, StudentAdminModel)
