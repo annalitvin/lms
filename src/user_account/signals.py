3ef289d7785fb22f563658f22de65074a734118c
annalitvin <litvin_any@ukr.net>
2020-06-17 11:05:26 +0300

    Added  styles in user_account app

--- src/user_account/signals.py
+++ src/user_account/signals.py
@@ -7,9 +7,7 @@ from user_account.models import UserAccountProfile
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, created, **kwargs):
-    if created:
+    if created or not hasattr(instance, 'profile'):
         UserAccountProfile.objects.create(user=instance)
-    # else:
-          #получается ошибка уникальности
-    #     profile = getattr(instance, 'profile', UserAccountProfile.objects.create(user=instance))
-    #     profile.save()
+    else:
+        instance.profile.save()
